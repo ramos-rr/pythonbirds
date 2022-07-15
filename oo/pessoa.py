3b632c6de79432673ea266aec9633951672a0723
ramos-rr <ramos-rr@outlook.com>
2022-07-14 21:40:43 -0300

    Criado atributos e intâncias em classe Pessoa()

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,16 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
 
+    def comprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('Luciano')
+    print(id(p))
+    print(p.nome)
+    p.nome = 'Rafael'
+    print(p.nome)
+    print(p.idade)
