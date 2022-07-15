cba7ed725785449cd6c9874018d5e28ccd5f25fe
ramos-rr <ramos-rr@outlook.com>
2022-07-15 09:30:57 -0300

    Criado atributo de classe 'olhos' em Pessoa()

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,7 @@
 class Pessoa:
-    def __init__(self, *filho, nome=None, idade=None):
+    olhos = 2 # atributo de classe
+
+    def __init__(self, *filho, nome=None, idade=None): #atributo de objeto
         self.filhos = list(filho)
         self.nome = nome
         self.idade = idade
@@ -21,8 +23,18 @@ if __name__ == '__main__':
     for f in luciano.filhos:
         print(f.nome)
     luciano.sobrenome = 'Ramalho'
-    print(luciano.nome, *luciano.__dict__.keys(), sep=', ')
-    print(renzo.nome, *renzo.__dict__.keys(), sep=', ')
+    print(renzo.__dict__)
+    print(luciano.__dict__)
     del luciano.sobrenome
-    print(luciano.nome, *luciano.__dict__.keys(), sep=', ')
-    print(renzo.nome, *renzo.__dict__.keys(), sep=', ')
+    luciano.olhos = 1
+    print(renzo.__dict__)
+    print(luciano.__dict__)
+    Pessoa.olhos = 3
+    del luciano.olhos
+    print(renzo.__dict__)
+    print(luciano.__dict__)
+    print(Pessoa.olhos)
+    print(id(Pessoa.olhos))
+    print(luciano.olhos)
+    print(id(luciano.olhos))
+    print(Pessoa.__dict__.keys())
